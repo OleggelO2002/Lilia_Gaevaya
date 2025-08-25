b7e1d9498532625bc00042aa587f35b5d637aa79
OleggelO2002 <134651410+OleggelO2002@users.noreply.github.com>
2025-08-25 14:48:36 +0700

    Update Banner.js

--- Banner.js
+++ Banner.js
@@ -1,22 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('=== Banner script started ===');
-
-  // Проверяем, есть ли уже баннер
-  const existingBanner = document.querySelector('.custom-banner');
-  if (existingBanner) {
-    console.log('Баннер уже есть, добавление отменено');
-    return;
-  }
-
-  console.log('Баннера нет, создаём...');
-
   // ====== СОЗДАЁМ БАННЕР ======
   const img = document.createElement('img');
-  const isMobile = window.innerWidth <= 768;
 
-  img.src = isMobile
-    ? 'https://optim.tildacdn.pub/tild3439-3361-4565-a137-663462363365/-/resize/785x/-/format/webp/Frame_312.png'
-    : 'https://static.tildacdn.com/tild3231-6535-4032-a430-396331383431/Frame_311.png';
+  const isMobile = window.innerWidth <= 768;
+  if (isMobile) {
+    img.src = 'https://optim.tildacdn.pub/tild3439-3361-4565-a137-663462363365/-/resize/785x/-/format/webp/Frame_312.png';
+  } else {
+    img.src = 'https://static.tildacdn.com/tild3231-6535-4032-a430-396331383431/Frame_311.png';
+  }
 
   img.alt = 'Banner Image';
   img.style.width = '100%';
@@ -29,17 +20,11 @@ document.addEventListener('DOMContentLoaded', function () {
   // ====== ДОБАВЛЯЕМ В DOM ======
   const container = document.querySelector('.container');
   if (container) {
-    console.log('Вставляем баннер в .container');
     container.insertBefore(bannerWrapper, container.firstChild);
   } else {
     const streamTable = document.querySelector('.xdget-root');
     if (streamTable) {
-      console.log('Вставляем баннер перед .xdget-root');
       streamTable.parentNode.insertBefore(bannerWrapper, streamTable);
-    } else {
-      console.log('Не найдено место для вставки баннера');
     }
   }
-
-  console.log('=== Banner script finished ===');
 });
