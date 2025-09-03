83ce67c50fda9c9a6993faf2d30d9b5d9d1312cf
OleggelO2002 <134651410+OleggelO2002@users.noreply.github.com>
2025-09-03 15:31:27 +0700

    Update trening-block-img.js

--- trening-block-img.js
+++ trening-block-img.js
@@ -1,11 +1,14 @@
 document.querySelectorAll('tr.training-row').forEach(row => {
   const imgUrl = row.dataset.trainingImage; // Берем URL из data-training-image
   if (imgUrl) {
+    // Заменяем 200x200 на 700x700 в ссылке
+    const highResUrl = imgUrl.replace(/\/s\/\d+x\d+\//, '/s/700x700/');
+
     const imgWrapper = document.createElement('div');
     imgWrapper.classList.add('training-image-wrapper');
 
     const img = document.createElement('img');
-    img.src = window.location.protocol + imgUrl; // добавляем https:
+    img.src = window.location.protocol + highResUrl; // добавляем https:
     img.alt = 'training image';
     img.classList.add('training-image');
 
